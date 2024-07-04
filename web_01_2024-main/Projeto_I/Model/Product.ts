f1f78473fea577d7229287da9ea86fe524a6be80
LYunoguthi <leandro.yunoguthi@hotmail.com>
2024-07-04 19:27:57 -0300

    sssssss

--- web_01_2024-main/Projeto_I/Model/Product.ts
+++ web_01_2024-main/Projeto_I/Model/Product.ts
@@ -4,7 +4,6 @@ export class Product{
     description:string;
     price:number;
     stock:number;
-    
 
     constructor(name:string, description:string, price:number){
         this.name = name;
