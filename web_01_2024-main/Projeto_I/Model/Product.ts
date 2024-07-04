c98c37deda42a5d0d8c55e6dc404041ba075c6ac
LYunoguthi <leandro.yunoguthi@hotmail.com>
2024-07-04 18:36:30 -0300

    projeto

---
+++ web_01_2024-main/Projeto_I/Model/Product.ts
@@ -0,0 +1,13 @@
+export class Product{
+    id:number;
+    name:string;
+    description:string;
+    price:number;
+    stock:number;
+
+    constructor(name:string, description:string, price:number){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.id = this.geraId();
+    }
