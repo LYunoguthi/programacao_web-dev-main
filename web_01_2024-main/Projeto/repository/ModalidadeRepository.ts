a89d84a47d54fa2c2b24890a252d15d6fea8b1dd
Leandro Yunoguthi <leandro.yunoguthi@hotmail.com>
2024-07-26 20:08:12 -0300

    3

--- web_01_2024-main/Projeto/repository/ModalidadeRepository.ts
+++ web_01_2024-main/Projeto/repository/ModalidadeRepository.ts
@@ -2,6 +2,18 @@ import { ModalidadeModel } from "../model/ModalidadeModel";
 
 export class ModalidadeRepository {
 
-    modalidadeList: ModalidadeModel[] = []
+    modalidadeList: ModalidadeModel[] = [];
+
+    insereProduto ( modalidadeLista : ModalidadeModel ) {
+        this . productList . push ( modalidadeLista ) ;
+         }
+        
+         filtraProdutoPorId ( id : number ) : Product | undefined {
+         return this . productList . find ( product = > product . id === id ) ;
+         }
+        
+         filtraTodosProdutos () : Product []{
+         return this . productList ;
+         }
 
 }
