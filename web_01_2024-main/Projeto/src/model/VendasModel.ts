b922cde125cb2002c424ef627b38812f2368aaa8
Leandro Yunoguthi <leandro.yunoguthi@hotmail.com>
2024-07-31 18:55:28 -0300

    33

--- web_01_2024-main/Projeto/src/model/VendasModel.ts
+++ web_01_2024-main/Projeto/src/model/VendasModel.ts
@@ -1,14 +1,18 @@
+import { ListaVendas } from "./ListaVendas";
+
 export class VendasModel{
     id:number;
     cpfcliente:number;
     valortotal:number;
     quantidade:number;
+    listaVendas:ListaVendas[];
 
-    constructor(id?:number, cpfcliente?:number, valortotal?:number, quantidade?:number){
+    constructor(id?:number, cpfcliente?:number, valortotal?:number, quantidade?:number, listaVendas?: ListaVendas[]){
         this.id = this.geraId() || 0;
         this.cpfcliente = cpfcliente || 0;
         this.valortotal = valortotal || 0;
         this.quantidade = quantidade || 0;
+        this.listaVendas = listaVendas || [];
     }
     
     private geraId():number{
