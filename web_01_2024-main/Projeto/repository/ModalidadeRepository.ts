4178e830756c602e850c6e67ccd2daf38bd55f9c
Leandro Yunoguthi <leandro.yunoguthi@hotmail.com>
2024-07-31 18:43:27 -0300

    3

--- web_01_2024-main/Projeto/repository/ModalidadeRepository.ts
+++ web_01_2024-main/Projeto/repository/ModalidadeRepository.ts
@@ -2,18 +2,37 @@ import { ModalidadeModel } from "../model/ModalidadeModel";
 
 export class ModalidadeRepository {
 
-    modalidadeList: ModalidadeModel[] = [];
+    modalidadeLista: ModalidadeModel[] = [];
 
     insereProduto ( modalidadeLista : ModalidadeModel ) {
-        this . productList . push ( modalidadeLista ) ;
-         }
+        this . modalidadeLista . push ( modalidadeLista ) ;
+    }
         
-         filtraProdutoPorId ( id : number ) : Product | undefined {
-         return this . productList . find ( product = > product . id === id ) ;
-         }
-        
-         filtraTodosProdutos () : Product []{
-         return this . productList ;
-         }
+    filtraProdutoPorId ( id : number ) : ModalidadeModel | undefined {
+        return this . modalidadeLista . find ( ModalidadeModel => ModalidadeModel.id === id ) ;
+    }
+
+    filtraProdutoPorNomeId(id: number, name:string): ModalidadeModel | undefined{
+        return this.modalidadeLista.find(ModalidadeModel => ModalidadeModel.name === name && ModalidadeModel.id === id);
+    }
+
+    filtraTodosProdutos () : ModalidadeModel []{
+        return this . modalidadeLista ;
+    }
+
+    deletaProduto(modalidadeLista:ModalidadeModel){
+        const index = this.modalidadeLista.indexOf(modalidadeLista);
+        if (index !== -1) {
+            this.modalidadeLista.splice(index, 1);
+        }
+    }
+
+    atualizaProduto(modalidadeLista:ModalidadeModel): number{
+        const index = this.modalidadeLista.indexOf(modalidadeLista);
+        if(index !== -1){
+            this.modalidadeLista[index] = modalidadeLista;
+        }
+        return index;
+    }
 
 }
